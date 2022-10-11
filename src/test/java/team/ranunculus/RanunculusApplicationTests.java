e0522513592613d2fa0b93ff3b8f7112695caff7
Gitae Gwon <ayy369@naver.com>
2022-10-11 16:14:26 +0900

    first

---
+++ src/test/java/team/ranunculus/RanunculusApplicationTests.java
@@ -0,0 +1,13 @@
+package team.ranunculus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RanunculusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
