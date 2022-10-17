76ee3f103b30e0a00c89d559cf9d1821ac99d279
Leehyun <srtleehyun@gmail.com>
2022-10-17 20:23:50 +0900

    SpringBoot Team Project Init
    - HomeController

---
+++ src/main/java/team/ranunculus/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package team.ranunculus.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller(value = "team.ranunculus.controllers.Controller")
+@RequestMapping(value = "/")
+public class HomeController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView getIndex(ModelAndView modelAndView) {
+        modelAndView.setViewName("home/index");
+        return modelAndView;
+    }
+}
