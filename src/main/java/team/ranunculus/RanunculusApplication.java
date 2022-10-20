c8bbc56adbbdad02974bf56ff45f2e3ca0ce2d01
hajuwon <dmpdpdpm@gmail.com>
2022-10-20 19:10:01 +0900

    edit space-between

---
+++ src/main/java/team/ranunculus/RanunculusApplication.java
@@ -0,0 +1,13 @@
+package team.ranunculus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RanunculusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RanunculusApplication.class, args);
+    }
+
+}
