ca35f40fbbb8330cd0e9482527653397b4e8ce1b
Gitae Gwon <ayy369@naver.com>
2022-10-20 21:43:53 +0900

    10-20 finish

---
+++ src/main/java/team/ranunculus/enums/member/UserLoginResult.java
@@ -0,0 +1,8 @@
+package team.ranunculus.enums.member;
+
+
+import team.ranunculus.interfaces.IResult;
+
+public enum UserLoginResult implements IResult {
+    SUSPENDED
+}
---
+++ src/main/java/team/ranunculus/interfaces/IResult.java
@@ -0,0 +1,6 @@
+package team.ranunculus.interfaces;
+
+public interface IResult {
+    String ATTRIBUTE_NAME = "result";
+    String ATTRIBUTE_NAME_PLURAL = "results";
+}
