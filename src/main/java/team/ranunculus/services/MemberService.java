a20a03678750c71e2b600e4956877de3fe01fee6
Hyun Lee <srtleehyun@gmail.com>
2022-10-21 23:47:19 +0900

    Spring Boot Team Project
    - hyun

--- src/main/java/team/ranunculus/services/MemberService.java
+++ src/main/java/team/ranunculus/services/MemberService.java
@@ -2,7 +2,7 @@ package team.ranunculus.services;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import team.ranunculus.entities.member.MemberEntity;
+import team.ranunculus.entities.member.UserEntity;
 import team.ranunculus.enums.CommonResult;
 import team.ranunculus.enums.member.UserLoginResult;
 import team.ranunculus.interfaces.IResult;
@@ -21,7 +21,7 @@ public class MemberService {
     }
 
     @Transactional
-    public IResult loginUser(MemberEntity member) throws NoSuchAlgorithmException {
+    public IResult loginUser(UserEntity member) throws NoSuchAlgorithmException {
         if (member.getEmail() == null ||
                 member.getPassword() == null ||
                 !member.getEmail().matches(MemberRegex.USER_EMAIL) ||
@@ -29,7 +29,7 @@ public class MemberService {
             return CommonResult.FAILURE;
         }
         member.setPassword(CryptoUtils.hashSha512(member.getPassword()));
-        MemberEntity existingMember = this.memberMapper.selectUserByEmailPassword(member);
+        UserEntity existingMember = this.memberMapper.selectUserByEmailPassword(member);
         if (existingMember == null) {
             return CommonResult.FAILURE;
         }
