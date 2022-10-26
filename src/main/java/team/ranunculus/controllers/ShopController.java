fa984e40f4eeec8e31d8260038114150af2b6293
Hyun Lee <srtleehyun@gmail.com>
2022-10-26 20:05:59 +0900

    Spring Boot Team Project
    - hyun

---
+++ src/main/java/team/ranunculus/controllers/ShopController.java
@@ -0,0 +1,18 @@
+package team.ranunculus.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(value = "/shop")
+public class ShopController {
+
+    @RequestMapping(value = "/category", method = RequestMethod.GET)
+    public ModelAndView getCategory(ModelAndView modelAndView) {
+        modelAndView.setViewName("shop/category");
+        return modelAndView;
+    }
+
+}
