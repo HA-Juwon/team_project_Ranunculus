88053b67cd0e991d519a7bb700598780ef77b589
bang-gilim <qkdrkans002@naver.com>
2022-11-17 19:40:35 +0900

    shop

--- src/main/java/team/ranunculus/controllers/ShopController.java
+++ src/main/java/team/ranunculus/controllers/ShopController.java
@@ -9,26 +9,24 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping(value = "/shop")
 public class ShopController {
 
+    private ModelAndView modelAndView;
+
     @RequestMapping(value = "/category", method = RequestMethod.GET)
     public ModelAndView getCategory(ModelAndView modelAndView) {
         modelAndView.setViewName("shop/category");
         return modelAndView;
     }
+
     @RequestMapping(value = "/basket", method = RequestMethod.GET)
     public ModelAndView getBasket(ModelAndView modelAndView) {
         modelAndView.setViewName("shop/basket");
         return modelAndView;
     }
+
     @RequestMapping(value = "/detail", method = RequestMethod.GET)
     public ModelAndView getDetail(ModelAndView modelAndView) {
         modelAndView.setViewName("shop/detailpage");
         return modelAndView;
     }
 
-
-    @RequestMapping(value = "/basket", method = RequestMethod.GET)
-    public ModelAndView getBasket(ModelAndView modelAndView) {
-        modelAndView.setViewName("shop/basket");
-        return modelAndView;
-    }
 }
